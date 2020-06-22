0b8eca7e77056267dc786c648a6425f48ead8635
DESKTOP-IGHA7RH\Adrian Gamarra <adriatogi@gmail.com>
2020-06-21 20:26:24 -0700

    Ruby Controller

---
+++ Assets/Scripts/RubyController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RubyController : MonoBehaviour
+{
+
+    private float _speed = 5.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        Vector2 position = transform.position;
+
+        position.x = position.x + _speed * horizontal * Time.deltaTime;
+        position.y = position.y + _speed * vertical * Time.deltaTime;
+        transform.position = position;
+    }
+}
