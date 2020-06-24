8d135a293ab36d6082015ff68a1b413e0232f6ab
Adrian Gamarra <Adriatogi@gmail.com>
2020-06-23 21:50:56 -0700

    Damage Zones and Enemies Pt. 2

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -5,20 +5,53 @@ using UnityEngine;
 public class EnemyController : MonoBehaviour
 {
     [SerializeField]
-    private float _speed;
+    private float _speed = 3.0f;
+    [SerializeField]
+    private bool vertical = false;
+    [SerializeField]
+    private float changeTIme = 3.0f;
 
     Rigidbody2D rigidBody2D;
+    int direction = 1;
 
     private void Start()
     {
         rigidBody2D = GetComponent<Rigidbody2D>();
+
+        StartCoroutine(DirectionRoutine());
     }
 
     private void FixedUpdate()
     {
         Vector2 position = rigidBody2D.position;
-        position.x = position.x + Time.deltaTime * _speed;
+
+        if (vertical)
+        {
+            position.y += Time.deltaTime * _speed * direction;
+        }
+        else
+        {
+            position.x += Time.deltaTime * _speed * direction;
+        }
 
         rigidBody2D.MovePosition(position);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        RubyController player = collision.gameObject.GetComponent<RubyController>();
+
+        if(player != null)
+        {
+            player.changeHealth(-1);
+        }
+    }
+    IEnumerator DirectionRoutine()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(changeTIme);
+            direction = -direction;
+        }
     }
 }
