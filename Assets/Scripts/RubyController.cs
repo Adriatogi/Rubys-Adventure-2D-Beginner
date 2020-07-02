89bbfb6e8380896ec67b8dc207dfa35b19e0311b
Adrian Gamarra <Adriatogi@gmail.com>
2020-07-01 23:32:23 -0700

    Dialog Raycast

--- Assets/Scripts/RubyController.cs
+++ Assets/Scripts/RubyController.cs
@@ -60,6 +60,21 @@ public class RubyController : MonoBehaviour
             Launch();
         }
 
+        if (Input.GetKeyDown(KeyCode.X))
+        {
+            RaycastHit2D hit = Physics2D.Raycast(rigidBody2D.position + Vector2.up * 0.2f, lookDirection, 1.5f, LayerMask.GetMask("NPC"));
+
+            if (hit.collider != null)
+            {
+                NonPlayerCharacter character = hit.collider.GetComponent<NonPlayerCharacter>();
+
+                if(character != null)
+                {
+                    character.DisplayDialog();
+                }
+            }
+        }
+
         animator.SetFloat("Look X", lookDirection.x);
         animator.SetFloat("Look Y", lookDirection.y);
         animator.SetFloat("Speed", move.magnitude);
